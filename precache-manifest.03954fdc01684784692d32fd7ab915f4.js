f29acf67117be60fbece3562c678edbfd7f1bbbb
Vaibhav Sinha <vaibhavleoanto@gmail.com>
2020-08-30 19:36:32 +0530

    Updates

---
+++ precache-manifest.03954fdc01684784692d32fd7ab915f4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fc6fae696d785c46308b0a4c89f06e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "140c9537382e24ee08c1",
+    "url": "/Portfolio/static/css/2.59266b1d.chunk.css"
+  },
+  {
+    "revision": "8403486c9c55de53cfa6",
+    "url": "/Portfolio/static/css/main.36373748.chunk.css"
+  },
+  {
+    "revision": "140c9537382e24ee08c1",
+    "url": "/Portfolio/static/js/2.cf59be00.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.cf59be00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8403486c9c55de53cfa6",
+    "url": "/Portfolio/static/js/main.787f8fa6.chunk.js"
+  },
+  {
+    "revision": "796235efddf27470bbfe",
+    "url": "/Portfolio/static/js/runtime-main.25f07560.js"
+  },
+  {
+    "revision": "371ee866a7e311f9e0ecadc2d73606b9",
+    "url": "/Portfolio/static/media/certificate.371ee866.png"
+  },
+  {
+    "revision": "8d91e4d06c07747dfa2f13c2a98289d3",
+    "url": "/Portfolio/static/media/fifa.8d91e4d0.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3cb2a9855189f5285ffa29ae926d8410",
+    "url": "/Portfolio/static/media/vaibh.3cb2a985.png"
+  }
+]);
